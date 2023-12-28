ab47b44ac578e502114898454ba397d09537e3c2
mathieu enault <me94110@gmail.com>
2023-12-28 18:04:27 +0100

    created navbar, carts and shops components and imported and used Libre Baskerville font

---
+++ src/components/Shop/ShopProductItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="w-25 p-3 product-container d-flex flex-column">
+    <div class="d-flex flex-row justify-content-center">
+      <img style="height: 150px" :src="product.img" />
+    </div>
+    <div>
+      <h3>{{ product.title }}</h3>
+      <hr />
+    </div>
+    <div>
+      <p>{{ product.description }}</p>
+    </div>
+    <div>
+      <span>{{ product.price }}</span>
+      <button class="btn btn-primary btn-sm float-right">Commander</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        img: "https://cdn.store-factory.com/www.confiserie-foraine.com/content/product_4936768b.jpg?v=1531989933",
+        title: "LUTTI bouteille Bubblizz ",
+        description: "Bonbons Lutti en vrac / sachet de 2 kg",
+        price: "13.00€",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.product-container {
+  height: 400px;
+}
+
+p {
+  font-size: 12px;
+}
+</style>
---
+++ src/components/TheHeader/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">
+      <img src="../../assets/candy-shop.png" width="50" height="50" />
+      CandyShop
+    </a>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link">Boutique</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Admin</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.navbar-nav > :hover {
+  cursor: pointer;
+}
+</style>
